afc64a98b497cb8c158de46273be00ad5aaee2f8
Anton Bondoc <antonbondoc14@gmail.com>
2023-06-12 23:44:51 +0800

    first commit

---
+++ src/main/java/com/antonbondoc/matatagrentalsystem/MatatagRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.antonbondoc.matatagrentalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatatagRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatatagRentalSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antonbondoc/matatagrentalsystem/MatatagRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonbondoc.matatagrentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatatagRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
