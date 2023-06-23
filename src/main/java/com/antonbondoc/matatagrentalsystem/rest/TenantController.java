dcee5c87db98df9f4be502d4f2bf9b0f8dc3b83b
Anton Bondoc <antonbondoc14@gmail.com>
2023-06-23 23:52:53 +0800

    feat: created initial controller for tenants

--- src/main/java/com/antonbondoc/matatagrentalsystem/rest/TenantController.java
+++ src/main/java/com/antonbondoc/matatagrentalsystem/rest/TenantController.java
@@ -1,20 +1,37 @@
 package com.antonbondoc.matatagrentalsystem.rest;
 
+import com.antonbondoc.matatagrentalsystem.core.TenantService;
+import com.antonbondoc.matatagrentalsystem.dto.request.TenantRequestDto;
+import com.antonbondoc.matatagrentalsystem.dto.response.TenantResponseDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/tenant")
+@RequiredArgsConstructor
 public class TenantController {
+
+    private final TenantService tenantService;
+
     @GetMapping("/hello")
     public String greet() {
         return "Hello World from Tenant";
     }
 
-    @GetMapping
-    public void getTenant() {}
+    @GetMapping(path = "{tenantId}")
+    public ResponseEntity<TenantResponseDto> getTenant(@PathVariable("tenantId") UUID tenantId) {
+        return ResponseEntity.ok(tenantService.getTenant(tenantId));
+    }
 
     @PostMapping
-    public void createTenant() {}
+    public ResponseEntity<TenantResponseDto> createTenant(@Valid @RequestBody TenantRequestDto request) {
+        return ResponseEntity.ok(tenantService.createTenant(request));
+    }
 
     @PutMapping
     public void updateTenant() {}
